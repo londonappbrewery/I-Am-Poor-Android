583df5e3acb81bd4189c17a81a46bd7315219a4e
londonappbrewery <londonappbrewery@gmail.com>
2017-07-05 15:15:32 +0100

    Fixed for the problems I caused....

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -2,8 +2,9 @@ package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
